4a2757a74abda50432e5f0af706e6510281b8130
suhail-z <suhail22rm@gmail.com>
2024-12-13 00:04:28 +0530

    todo list app using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {React,useRef,useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+
+  const [doList,setDoList]=useState([]);
+  const [work,setWork]=useState('');
+  const [workIndex,setWorkIndex]=useState(null);
+
+  function updateList(){
+
+    //acts as update button  
+    if(workIndex !== null){
+
+      setDoList(list=>{
+        return list.map((task,index)=>{
+          return index === workIndex ? work : task ;
+      })
+    })
+    setWorkIndex(null);
+    }
+
+    //acts as a add button - add new work
+    else
+    {
+    setDoList(list=>[...list,work]);
+    }
+    setWork('');
+
+    }
+
+  function deleteFromList(index){
+    setDoList((list)=> list.filter((_,ind)=> ind!=index))
+  } 
+
+  function editList(ind){
+    setWorkIndex(ind);
+    setWork(doList[ind])
+  }
 
   return (
-    <>
+  <>
+  <input type="text" className='works' placeholder='Type to add a task' value={work} onChange={e => setWork(e.target.value)} onKeyDown={(e)=>{
+    if(e.key==='Enter'){
+      updateList();
+    }
+  }}/>
+  <button className='add-btn' disabled={!work.trim()} onClick={updateList}>
+    {workIndex === null? 'Add' : 'Update' }
+  </button>
+  <hr/>
+  <div>
+    {
+    doList.length === 0 ?(
+    <p>No Work to be done</p> ):(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {
+        doList.map((work,index)=>(
+        (
+        
+        <p className='list' key={index}>
+          {work}
+        
+        <button className='update-btn' onClick={()=>editList(index)}> |=| </button>
+        <button className='delete-btn' onClick={()=>deleteFromList(index)}> \_/ </button>
       </p>
-    </>
+      
+        )
+    ))}
+      </div>
   )
 }
+  </div>
+  </>
+)
+}
 
-export default App
+export default App;
