22b3768a545c22c53164cbce0d8892bc8375505e
suhail-z <suhail22rm@gmail.com>
2025-03-23 16:03:50 +0530

    task app using useReducer

--- pro2/src/App.jsx
+++ pro2/src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useReducer, useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reduce(state,action){
+  switch(action.to){
+    case 'add':
+      return [...state,{id:action.payload.id,name:action.payload.name}];
+    case 'delete':
+      return state.filter(task=> task.id!==action.payload.id);
+    case 'update':
+        return state.map(task=>
+          task.id === action.payload.id ?
+          {...task,name:action.payload.name}:
+          task
+        );
+    case 'clear':
+      return  state.map(task=>
+        task.id === action.payload.id ?
+        {...task,clear:true}:
+        task
+      );
+    default:
+      return state;    
+  }
 }
 
-export default App
+  function init(){
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+  }
+export const App = () => {
+  const [editId,setEditId] = useState(null);
+  const [state,dispatch] = useReducer(reduce,[],init);
+  const [task,setTask] = useState('');
+
+
+  useEffect(()=>{
+    localStorage.setItem('tasks',JSON.stringify(state));
+  },[state])
+
+const addTask=()=>{
+  task.trim() !== '' ? dispatch({to:'add',payload:{id:Date.now(),name:task,clear:false}}):alert('Enter task before submitting');
+  setTask('');
+}
+const editTask=(task)=>{
+  setTask(task.name);
+  setEditId(task.id);
+  
+}
+const update=()=>{
+  dispatch({to:"update",payload:{id:editId,name:task}});
+  setTask('');
+  setEditId(null);
+}
+const deleteTask=(taskId)=>{
+  dispatch({to:'delete',payload:{id:taskId}})
+}
+const clearTask = (id)=>{
+  dispatch({to:'clear',payload:{id:id}})
+}
+
+  return <>
+    <div className="container">
+      <h3>TASKS : {state.length}</h3>
+      <div className="input-sec">
+
+        <input type="text" className="input" value={task}  
+        onChange={e=>setTask(e.target.value)} 
+        onKeyDown={e=> {if(e.key==='Enter')
+        {editId? update() :addTask()}
+        }}/>
+
+        <button className="add-btn" onClick={editId ? update :addTask}>
+          {editId ? 'Update' : 'Add Task'}
+        </button>
+      </div>
+      <div className="display">
+        <ol>
+        {
+          state.map(task=>(
+            <li key={task.id} >
+
+                {task.clear ? <s>{task.name}</s>:task.name}
+                <div>
+                <button className="update" disabled={task.clear} onClick={()=>editTask(task)}>Edit</button>
+                <button className="delete" onClick={()=>deleteTask(task.id)}>\_/</button>
+                <button className="clear" disabled={task.clear} onClick={()=>clearTask(task.id)}>X</button>
+                </div>
+              
+                 
+
+            </li>
+          ))
+        }
+        </ol>
+      </div>
+    </div>
+  </>
+}
+export default App;
